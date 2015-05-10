5a0614e72e705b2b2b5d601d8a4c731bbec2eb18
LIU Dongyuan / 柳东原 <liu.dongyuan@gmail.com>
2015-05-09 17:03:47 -0700

    Add TagView

---
+++ TagListView/TagView.swift
@@ -0,0 +1,31 @@
+//
+//  TagView.swift
+//  TagListViewDemo
+//
+//  Created by Dongyuan Liu on 2015-05-09.
+//  Copyright (c) 2015 Ela. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class TagView: UIButton {
+    
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        didSet {
+            layer.cornerRadius = cornerRadius
+            layer.masksToBounds = cornerRadius > 0
+        }
+    }
+    @IBInspectable var borderWidth: CGFloat = 0 {
+        didSet {
+            layer.borderWidth = borderWidth
+        }
+    }
+    @IBInspectable var borderColor: UIColor? {
+        didSet {
+            layer.borderColor = borderColor?.CGColor
+        }
+    }
+
+}
