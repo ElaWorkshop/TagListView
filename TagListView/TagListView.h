f54970ce186b7579d1dcda5aabcb278aa5b803ae
Naoki Morita <namorit@gmail.com>
2015-09-11 01:48:19 +0900

    Add TagListView.h

---
+++ TagListView/TagListView.h
@@ -0,0 +1,18 @@
+//
+//  TagListView.h
+//  TagListView
+//
+//  Created by MORITANAOKI on 2015/09/11.
+//  Copyright (c) 2015年 Ela. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TagListView.
+FOUNDATION_EXPORT double TagListViewVersionNumber;
+
+//! Project version string for TagListView.
+FOUNDATION_EXPORT const unsigned char TagListViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TagListView/PublicHeader.h>
+
