919a60f9c49aaecc93774b68abec09a68b427ea9
Ben Wu <benwu00@gmail.com>
2016-02-11 17:51:34 -0500

    Consolidate a CloseButton class with DrawCloseView class

--- TagListViewDemo/CloseView.swift
+++ TagListViewDemo/CloseView.swift
@@ -8,65 +8,35 @@
 
 import UIKit
 
-class CloseButton: UIButton {
-
-    var closeView = DrawCloseView()
+internal class CloseButton: UIButton {
 
     var iconSize: CGFloat = 10
-    var lineWidth: CGFloat = 1 {
-        didSet {
-            closeView.lineWidth = lineWidth
-        }
-    }
-    var lineColor: UIColor = UIColor.whiteColor().colorWithAlphaComponent(0.54) {
-        didSet {
-            closeView.lineColor = lineColor
-        }
-    }
+    var lineWidth: CGFloat = 1
+    var lineColor: UIColor = UIColor.whiteColor().colorWithAlphaComponent(0.54)
 
     weak var tagView: TagView?
 
-    init() {
-        super.init(frame: CGRectZero)
-        setup()
-        closeView.backgroundColor = UIColor.clearColor()
-        closeView.userInteractionEnabled = false
-    }
-
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-
-    func setup() {
-        addSubview(closeView)
-    }
-
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        closeView.frame.size = CGSize(width: iconSize, height: iconSize)
-        closeView.center = CGPoint(x: self.frame.width/2, y: self.frame.height/2)
-    }
-}
-
-public class DrawCloseView: UIView {
-
-    var lineWidth: CGFloat = 1
-
-    var lineColor = UIColor.whiteColor().colorWithAlphaComponent(0.54)
-
-    public override func drawRect(rect: CGRect) {
+    override func drawRect(rect: CGRect) {
         let path = UIBezierPath()
 
         path.lineWidth = lineWidth
         path.lineCapStyle = .Round
 
-        path.moveToPoint(CGPoint(x: 0, y: 0))
-        path.addLineToPoint(CGPoint(x: rect.width, y: rect.height))
-        path.moveToPoint(CGPoint(x: rect.width, y: 0))
-        path.addLineToPoint(CGPoint(x: 0, y: rect.height))
+        let iconFrame = CGRect(
+            x: (rect.width - iconSize) / 2.0,
+            y: (rect.height - iconSize) / 2.0,
+            width: iconSize,
+            height: iconSize
+        )
+
+        path.moveToPoint(iconFrame.origin)
+        path.addLineToPoint(CGPoint(x: iconFrame.maxX, y: iconFrame.maxY))
+        path.moveToPoint(CGPoint(x: iconFrame.maxX, y: iconFrame.minY))
+        path.addLineToPoint(CGPoint(x: iconFrame.minX, y: iconFrame.maxY))
 
         lineColor.setStroke()
 
         path.stroke()
     }
+
 }
