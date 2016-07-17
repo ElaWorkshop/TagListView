14fdf439b6c16c0cd8dc4c27c1f25fb57b23ec1f
Aaron Pearce <me@aaronpearce.com>
2016-07-17 20:57:38 +1200

    Invalidate intrinsic content size on rearrange of views (#63)

--- TagListView/TagListView.swift
+++ TagListView/TagListView.swift
@@ -269,6 +269,8 @@ open class TagListView: UIView {
             currentRowView.frame.size.height = max(tagViewHeight, currentRowView.frame.height)
         }
         rows = currentRow
+        
+        invalidateIntrinsicContentSize()
     }
     
     // MARK: - Manage tags
