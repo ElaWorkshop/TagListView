7d9d50cf213fa155f625e5297156ab5f3c8a11c0
Arnaud Leene <aleene@gmail.com>
2016-12-04 16:16:55 +0100

    Update ViewController.swift
    spaces removed

--- TagListViewDemo/ViewController.swift
+++ TagListViewDemo/ViewController.swift
@@ -5,10 +5,11 @@
 //  Created by Dongyuan Liu on 2015-05-09.
 //  Copyright (c) 2015 Ela. All rights reserved.
 //
+
 import UIKit
 
 class ViewController: UIViewController, TagListViewDelegate {
-    
+
     @IBOutlet weak var tagListView: TagListView!
     @IBOutlet weak var biggerTagListView: TagListView!
     @IBOutlet weak var biggestTagListView: TagListView!
