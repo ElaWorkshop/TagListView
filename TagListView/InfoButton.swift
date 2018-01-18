97bf8dc73da3006d01a48dec4ce45cb8329db690
João Borges <sucrewar10@gmail.com>
2018-01-18 15:10:47 +0000

    Update InfoButton.swift
    removed old method to draw x in button

--- TagListView/InfoButton.swift
+++ TagListView/InfoButton.swift
@@ -15,27 +15,4 @@ internal class InfoButton: UIButton {
     var lineColor: UIColor = UIColor.white.withAlphaComponent(0.54)
     
     weak var tagView: TagView?
-    
-    override func draw(_ rect: CGRect) {
-        let path = UIBezierPath()
-        
-        path.lineWidth = lineWidth
-        path.lineCapStyle = .round
-        
-        let iconFrame = CGRect(
-            x: (rect.width - iconSize) / 2.0,
-            y: (rect.height - iconSize) / 2.0,
-            width: iconSize,
-            height: iconSize
-        )
-        
-        path.move(to: iconFrame.origin)
-        path.addLine(to: CGPoint(x: iconFrame.maxX, y: iconFrame.maxY))
-        path.move(to: CGPoint(x: iconFrame.maxX, y: iconFrame.minY))
-        path.addLine(to: CGPoint(x: iconFrame.minX, y: iconFrame.maxY))
-        
-        lineColor.setStroke()
-        
-        path.stroke()
-    }
 }
