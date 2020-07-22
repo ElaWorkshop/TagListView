ef1510ca987017d09415ea769af6e92a24e6ac31
kapizoli77 <kapi.zoli77@gmail.com>
2020-07-22 23:11:25 +0200

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,12 @@
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(name: "TagListView",
+                      platforms: [.iOS(.v8)],
+                      products: [.library(name: "TagListView",
+                                          targets: ["TagListView"])],
+                      targets: [.target(name: ""TagListView",
+                                        path: "TagListView")],
+                      swiftLanguageVersions: [.v5])
