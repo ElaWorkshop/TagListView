ab4df3b3ae8f1d8aa319d73e443c76545132fe17
soadelhayek <dev.shayek@gmail.com>
2023-09-26 22:50:17 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "TagListView",
+            name: "TagListView_niceone",
             targets: ["TagListView"]),
     ],
     targets: [
